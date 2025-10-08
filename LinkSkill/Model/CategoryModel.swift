19f3befda10ead4f5d9379c1420ca9b66680895b
e180369 <rohitsingh.dhakad@mastercard.com>
2025-10-09 01:44:20 +0530

    Job Detail APi Integrated

---
+++ LinkSkill/Model/CategoryModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryModel.swift
+//  LinkSkill
+//
+//  Created by Rohit Singh Dhakad  [C] on 09/10/25.
+//
+
+import UIKit
+
+class CategoryModel: NSObject {
+
+}
---
+++ LinkSkill/ViewControllers/Home/JobDetailDescriptionServicesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  JobDetailDescriptionServicesTableViewCell.swift
+//  LinkSkill
+//
+//  Created by Rohit Singh Dhakad  [C] on 09/10/25.
+//
+
+import UIKit
+
+class JobDetailDescriptionServicesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
